72806573f04aedf89d7996b53c88f4c51cf144fc
DOME-Architects <dome.architects2025@gmail.com>
2025-11-01 20:27:08 +0000

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const hero = document.querySelector('.hero');
 const heroImages = [
   'images/hero1.jpg',
   'images/hero2.jpg',
-  'images/hero3.jpg'
+  'images/hero3.jpeg'
 ];
 
 let currentIndex = 0;
