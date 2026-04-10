fc7edfc1952a76eb41baebdd09546d0352ec5dde
DOME-Architects <dome.architects2025@gmail.com>
2026-04-10 20:31:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,20 @@
-const slides = [
+const hero = document.querySelector('.hero');
+
+const images = [
   'images/hero1.jpg',
   'images/hero2.jpg',
   'images/hero3.jpeg'
 ];
 
-let current = 0;
-let slide1 = document.querySelector('.slide1');
-let slide2 = document.querySelector('.slide2');
-let toggle = true;
+let index = 0;
 
-slide1.style.backgroundImage = `url(${slides[0]})`;
-slide1.classList.add('active');
-slide2.style.backgroundImage = `url(${slides[1]})`;
+// initial image
+hero.style.backgroundImage = `url(${images[0]})`;
 
 setInterval(() => {
-  toggle = !toggle;
-
-  if (toggle) {
-    slide1.style.backgroundImage = `url(${slides[current]})`;
-    slide1.classList.add('active');
-    slide2.classList.remove('active');
-  } else {
-    slide2.style.backgroundImage = `url(${slides[current]})`;
-    slide2.classList.add('active');
-    slide1.classList.remove('active');
-  }
-
-  current = (current + 1) % slides.length;
+  index = (index + 1) % images.length;
+  hero.style.backgroundImage = `url(${images[index]})`;
 }, 5000);
-
-
-
 
 
 
